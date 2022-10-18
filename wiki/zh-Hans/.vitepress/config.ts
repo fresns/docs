f6ff2a6267d9cc35aea7c0c6038788ea2be75a97
Jarvis <jarvis.okay@gmail.com>
2022-10-19 00:49:28 +0800

    chore(wiki): algolia config

--- wiki/zh-Hans/.vitepress/config.ts
+++ wiki/zh-Hans/.vitepress/config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
 
         algolia: {
             appId: 'IWES74ZEKI',
-            apiKey: 'be9776b7fd35520c1f3ff6ef7be97703',
+            apiKey: 'ca90647551baab10e88397dc8b58506c',
             indexName: 'fresns_cn'
         }
     }
