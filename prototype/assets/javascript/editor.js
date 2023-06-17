488a1601a245cc5919d038fbedd79354b254be83
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-06-17 21:01:40 +0800

    chore: file

--- prototype/assets/javascript/editor.js
+++
@@ -1,5 +0,0 @@
-/* Tooltips */
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-});
